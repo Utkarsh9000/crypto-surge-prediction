5f1e8b3cd243be364ab982f395880346d75ab284
Utkarsh9000 <us9659@srmist.edu.in>
2026-03-17 15:25:14 +0530

    Fix import path for Vercel

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+ROOT = os.path.dirname(__file__)
+SRC = os.path.join(ROOT, "src")
+if SRC not in sys.path:
+    sys.path.insert(0, SRC)
+
 from cryptosurge.api import app
 
 __all__ = ["app"]
